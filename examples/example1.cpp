04c3583e631bd66e7f1418ced82f264a8cb5bf3b
NoX <128933018+NN0X@users.noreply.github.com>
2024-04-08 14:29:11 +0200

    VS project
    Added vs project in examples and renamed files

---
+++ examples/example1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+#include <AF.h>
+
+int main()
+{
+    Application *app = AF::init();
+    // Application *app = new Application();
+
+    int frames = 0;
+    double start = AF::getTime(app);
+
+    // app->loadContext("../resources/database/default.ndl");
+
+    while (AF::isRunning(app))
+    {
+        if (AF::Input::isKeyPressed(app, AF::Input::KEY_ESCAPE))
+            AF::setIsRunning(app, false);
+
+        AF::update(app);
+        frames++;
+    }
+
+    double end = AF::getTime(app);
+    double sum = end - start;
+
+    AF::quit(app);
+
+    std::cout << "Total Frames:    " << frames << "\n";
+    std::cout << "Total Time:      " << sum << "s\n";
+    std::cout << "Average FPS:     " << frames / sum << "\n";
+    std::cin.get();
+}
